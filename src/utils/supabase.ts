be49f80af0c3b5821b9ec757fc4e2cfbce3b4bcf
cdevelopment010 <cdevelopment010@gmail.com>
2025-03-03 15:58:14 +0000

    Feat: Supabase client auth options

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -6,4 +6,9 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY){
     throw new Error("Missing Supabase environment variables.");
 }
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true
+    }
+});
