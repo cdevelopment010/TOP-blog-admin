9e40b307363e6dfc45d284d387be3bc97e2862af
cdevelopment010 <cdevelopment010@gmail.com>
2025-03-03 15:11:25 +0000

    Test: add  auth check to redirect to sign-in

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,7 +30,7 @@ router.beforeEach((to, from, next) => {
     console.log("!isAuthenticated()",!isAuthenticated())
     if (to.meta.requiresAuth && !isAuthenticated() ) {
     //     // console.log("HERE", from);
-    //     next({ name: 'sign-in'}); 
+        next({ name: 'sign-in'}); 
     } else {
         next(); 
     }
